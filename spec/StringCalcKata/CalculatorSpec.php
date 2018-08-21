50caab554521f43ada2923d4cf822a29f10d2ee9
Sam-Burns <sam@sam-burns.com>
2018-08-21 14:10:43 +0100

    Minor test tweak

--- spec/StringCalcKata/CalculatorSpec.php
+++ spec/StringCalcKata/CalculatorSpec.php
@@ -15,13 +15,13 @@ class CalculatorSpec extends ObjectBehavior
 
     function it_returns_zero_for_an_empty_string(InputProcessor $inputProcessor)
     {
-        $inputProcessor->toInts('')->willReturn([]);
+        $inputProcessor->toInts('')->shouldBeCalled()->willReturn([]);
         $this->calculate('')->shouldBeLike(new Result(0));
     }
 
     function it_can_add_two_numbers(InputProcessor $inputProcessor)
     {
-        $inputProcessor->toInts('1,1')->willReturn([1,1]);
+        $inputProcessor->toInts('1,1')->shouldBeCalled()->willReturn([1,1]);
         $this->calculate('1,1')->shouldBeLike(new Result(2));
     }
 }
